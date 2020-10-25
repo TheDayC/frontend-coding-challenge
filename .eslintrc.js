b03627497e4896a6ae0296bd46dcac89acd77d8f
TheDayC <daveconnolly90@gmail.com>
2020-10-25 11:18:55 +0000

    Added eslint with some rules that I prefer for code styling

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        'amd': true,
+        'node': true,
+        'es6': true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        'indent': [
+            'warning',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        '@typescript-eslint/explicit-module-boundary-types': ['error', {
+            'allowDirectConstAssertionInArrowFunctions': true
+        }],
+        'react/prop-types': 0
+    },
+    'settings': {
+        'react': {
+            'version': '16.12.0'
+        }
+    }
+};
