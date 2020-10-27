b5639330e95a869cddb2d7981e52d67d6b1ff819
TheDayC <daveconnolly90@gmail.com>
2020-10-27 20:59:47 +0000

    Main selector for search term

--- src/selectors/index.ts
+++ src/selectors/index.ts
@@ -1,3 +1,4 @@
 import { IAppState } from '../types/store';
 
 export const selectTournaments = (state: IAppState) => state.tournaments;
+export const selectSearchTerm = (state: IAppState) => state.search;
